9353cfb9711877c8d6a9442851733319adcdefdc
Joe Bright <joe.bright@physics.ox.ac.uk>
2024-05-24 08:58:27 +0100

    mstransform scan fixes

--- ATARI/01_setup.py
+++ ATARI/01_setup.py
@@ -82,7 +82,7 @@ if os.path.isdir(mydata) == True and mydata.endswith('.ms') == False and mydata.
     f.write('concat(vis=' + str(final_concat) + ', concatvis=\'' + mydata + '/master_ms_tmp.ms\')')
     f.close()
     os.system(casapath + ' --nologger --log2term --nologfile -c concat_command.py')
-    fix_scans.fix_scans(mydata)
+    fix_scans.fix_scans(mydata + '/master_ms_tmp.ms')
     f = open('avg_command.py', 'w')
     f.write('mstransform(vis=\'' + mydata + '/master_ms_tmp.ms\', outputvis=\'' + mydata + '/master_ms.ms\', chanaverage=True, chanbin=' + chanbin + ' , datacolumn=\'DATA\')')
     f.close()
