e89f44d9cef4c2ae3a78e88c7e32fac713d4af40
Joe Bright <joe.bright@physics.ox.ac.uk>
2024-05-25 05:57:23 +0100

    Faster rerunning.

--- ATARI/01_setup.py
+++ ATARI/01_setup.py
@@ -81,6 +81,8 @@ if os.path.isdir(mydata) == True and mydata.endswith('.ms') == False and mydata.
         fields.append(file.split('/')[-1].split('_')[4])
     unique_fields = list(set(fields))
 
+    print(fields)
+
     if __name__ == '__main__':
         with multiprocessing.Pool() as pool:
             results = pool.map(parallel_file_conversion, unique_fields)
